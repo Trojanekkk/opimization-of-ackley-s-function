feb4bb55706128a688ab27bec924092818ee730b
Trojanekkk <W.Trojan27@gmail.com>
2021-04-20 17:50:56 +0200

    Add ackley's contour; Rewrite ackley's calculation with numpy vars

--- src/main.py
+++ src/main.py
@@ -4,32 +4,37 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # Calculate ackley 
-def ackley(range_array):
-    res = np.empty([len(range_array[0])])
+def ackley(X, Y):
+    Z = np.zeros((len(X), len(Y)))
     
-    for i in range(len(range_array[0])):
-        factor_1 = -0.02 * np.sqrt(0.5 * (range_array[0][i] * range_array[0][i] + range_array[1][i] * range_array[1][i]))
-        factor_2 = 0.5 * (np.cos(2 * np.pi * range_array[0][i]) + np.cos(2 * np.pi * range_array[1][i]))
-        
-        res[i] = -20 * np.exp(factor_1) - np.exp(factor_2) + np.exp(1) + 20
+    factor_1 = -0.02 * np.sqrt(0.5 * (X * X + Y * Y))
+    factor_2 = 0.5 * (np.cos(2 * np.pi * X + np.cos(2 * np.pi * Y)))
+    
+    Z = -20 * np.exp(factor_1) - np.exp(factor_2) + np.exp(1) + 20
 
-    return res
+    return Z
 
-# Plot results
-def plot_ackley(x1_range,x2_range):
-    range_array = [x1_range,x2_range]
-    z_range = ackley(range_array)
+# Define plotting ackley
+def plot_ackley (X, Y, Z):
     fig = plt.figure()
     ax = Axes3D(fig)
-    ax.scatter(x1_range, x2_range, z_range, label='Ackley Function')
+    ax.scatter(X, Y, Z, label='Ackley Function')
+    plt.show()
+
+# Define plotting ackley contour
+def plot_ackley_contour (X, Y, Z):
+    ax = plt.axes()
+    ax.contour(X, Y, Z)
 
 # Prepare range
-x1_range = []
-x2_range = []
+wek_x = np.arange(-35,36,1)
+wek_y = np.arange(-35,36,1)
 
-for x in np.linspace(-35, 35, 71):
-    for y in np.linspace(-35, 35, 71):
-        x1_range.append(x)
-        x2_range.append(y)
-    
-plot_ackley(x1_range ,x2_range)
+X, Y = np.meshgrid(wek_x, wek_y)
+
+# Calculate ackley and plot
+Z = ackley(X, Y)
+
+plot_ackley(X, Y, Z)
+
+plot_ackley_contour(X, Y, Z)
