e7ab38170597295562dd180ededdf63fd398254b
Wojciech Trojanowski <W.Trojan27@gmail.com>
2021-06-06 12:50:28 +0200

    Add Genetic Algorithm

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # Import packages
 import numpy as np
+from random import shuffle, randint
 from matplotlib import cm
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -110,6 +111,49 @@ def grad_descent(start_point, tolerance, step_factor, max_iter, h):
         'max_iter_r': (False, True)[iter == max_iter]
     }
 
+# Find minimum using Genetic Algorithm (GA)
+def genetic_algorithm (init_population, inheritance_power, bastard_probability, n_generation):
+
+    # Init population and calculate value of individuals
+    population = []
+    for coord in init_population:
+        population.append({'coord': np.array(coord), 'value': ackley_scalar(coord[0], coord[1])})
+
+    # Evolute generations
+    for generation in range(0, n_generation):
+
+        # Randomly mix individuals
+        shuffle(population)
+
+        # Divide population and groupe in couples
+        females = population[0:int(len(population)/2)]
+        males = population[int(len(population)/2):]
+
+        grouped_population = list(zip(females, males))
+        population = []
+
+        # For every couple create two children - reproduction
+        for couple in grouped_population:
+            worse = max(couple, key=lambda c: c['value'])
+            better = min(couple, key=lambda c: c['value'])
+
+            # Create new DNA, mutate the DNA if the children is a bastard
+            vector = np.array([(better['coord'][0] - worse['coord'][0]) * (-1,1)[randint(0,100) > 100 * bastard_probability], (better['coord'][1] - worse['coord'][1]) * (-1,1)[randint(0,100) > 100 * bastard_probability]])
+
+            # Inject the DNA to a new child
+            child_2_coord = worse['coord'] + vector * inheritance_power
+
+            # Create a new population - succession
+            population.extend([better,{'coord': child_2_coord, 'value': ackley_scalar(child_2_coord[0], child_2_coord[1])}])
+
+    # Select best solution final population
+    solution = min(population, key=lambda c: c['value'])
+    return {
+        'best_coord': solution['coord'],
+        'best_value': solution['value'],
+        'iterations': n_generation
+    }
+
 # Prepare range
 wek_x = np.arange(-35,36,1)
 wek_y = np.arange(-35,36,1)
@@ -127,16 +171,26 @@ plot_ackley_contour(X, Y, Z)
 SP = np.array([35.0,-32.0])     # Starting position 
 SL = 6.5                        # Initial step length
 E = 0.01                        # Tolerance, minimum step length
-SF = 0.3                        # Step factor, step length reduction 
+SF = 0.3                       # Step factor, step length reduction 
 
 # Run Hook-Jeeves algorithm
 result = hooke_jeeves(SP, SL, E, SF)
-print(f"Found minimum at {result['best_coord']} with value: {result['best_value']}, it took {result['iterations']} iterations.")
+print(f"Using Hooke-Jeeves algorithm found minimum at {result['best_coord']} with value: {result['best_value']}, it took {result['iterations']} iterations.")
 
 # cont. Init variables for searching minimum
-h = 0.00001                       # Derivative distance factor 
+h = 0.00001                     # Derivative distance factor 
 MI = 600                        # Maximum allowed iterations (GD)
 
 # Run Gradient Descent algorithm
 result = grad_descent(SP, E, SF, MI, h)
-print(f"Found minimum at {result['best_coord']} with value: {result['best_value']}, it took {result['iterations']} iterations. ", ('','Maximum iterations reached')[result['max_iter_r']])
+print(f"Using Gradient Descent algorithm found minimum at {result['best_coord']} with value: {result['best_value']}, it took {result['iterations']} iterations. ", ('','Maximum iterations reached')[result['max_iter_r']])
+
+# cont. Init variables for GA                                
+SPs = np.array([[35.0,-32.0],[-17.5,19.0],[-9.5,11.0],[-32.0,-21.0],[-7.5,-13.0],[28.0,11.5]])
+                                # Starting population
+BP = 0.2                        # Bastardness (mutation occure) probability
+N_GEN = 50                      # Number of generations to simulate
+
+# Run Genetic Algorithm
+result = genetic_algorithm(SPs, SF, 0.2, 50)
+print(f"Using Genetic Algorithm found minimum at {result['best_coord']} with value: {result['best_value']}, it took {result['iterations']} iterations.")
